ebf570741dcea8826a35b6d3735a33a01d40a9fb
Daniel Valenzuela <DanielValenzuelaPerez@outlook.com>
2022-12-26 15:18:19 -0600

    Added basic home template.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from accounts.models import Account
 
-# Create your views here.
+def home_view(request):
+    account = Account.objects.get(id=1)
+    context = {
+        'id': account.id,
+        'handle': account.handle
+    }
+    HTML_STRING = render_to_string('home-view.html', context=context)
+    return HttpResponse(HTML_STRING)
